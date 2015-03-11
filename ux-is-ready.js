5a9567f2d4161345671a35f26925a8b0dfd9689a
ʞɐǝɹɟʞǝǝƃ <davey@geekfreak.com>
2015-03-11 14:18:38 +0100

    updated ga

--- ux-is-ready.js
+++ ux-is-ready.js
@@ -256,20 +256,3 @@ skel.registerPlugin("layers",function(e){function dn(e,r,i){var o,u;this.id=e,th
   $( initialize );
 
 })(jQuery);
-
-<!-- Analytics ============================================================= -->
-( function( i, s, o, g, r, a, m ) {
-  i[ "GoogleAnalyticsObject" ] = r;
-  i[ r ] = i[ r ] || function() {
-    ( i[ r ].q = i[ r ].q || [] ).push( arguments );
-  }, i[ r ].l = 1 * new Date();
-  a = s.createElement( o ),
-  m = s.getElementsByTagName( o )[ 0 ];
-  a.async = 1;
-  a.src = g;
-  m.parentNode.insertBefore( a, m );
-} )( window, document, "script", "https://www.google-analytics.com/analytics.js", "ga" );
-ga( "create", "UA-49766755-1", "uxisready.com" );
-ga( "send", "pageview" );
-<!-- ======================================================================= -->
-
